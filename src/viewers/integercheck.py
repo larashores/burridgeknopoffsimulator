d5fcfc4aa33a8d4b77dd198c08cb31513056d391
Vincent <themilkmentaskforce@gmail.com>
2018-10-13 19:21:54 -0400

    Can edit viewing variables mid view

---
+++ src/viewers/integercheck.py
@@ -0,0 +1,145 @@
+def int_validate(entry_widget, limits=(None, None)):
+    """
+    Validates an entry_widget so that only integers within a specified range may be entered
+
+    :param entry_widget: The tkinter.Entry widget to validate
+    :param limits: The limits of the integer. It is given as a (min, max) tuple
+
+    :return:       None
+    """
+    num_str = entry_widget.get()
+    current = None if (not _is_int(num_str)) else int(num_str)
+    check = _NumberCheck(entry_widget, limits[0], limits[1], current=current)
+    entry_widget.config(validate='all')
+    entry_widget.config(validatecommand=check.vcmd)
+    entry_widget.bind('<FocusOut>', lambda event: _validate(entry_widget, check))
+    _validate(entry_widget, check)
+
+
+def _is_int(num_str):
+    """
+    Returns whether or not a given string is an integer
+
+    :param num_str: The string to test
+
+    :return: Whether or not the string is an integer
+    """
+    try:
+        int(num_str)
+        return True
+    except ValueError:
+        return False
+
+
+def _validate(entry, num_check):
+    """
+    Validates an entry so if there is invalid text in it it will be replaced by the last valid text
+
+    :param entry: The entry widget
+    :param num_check: The _NumberCheck instance that keeps track of the last valid number
+
+    :return:    None
+    """
+    if not _is_int(entry.get()):
+        entry.delete(0, tk.END)
+        entry.insert(0, str(num_check.last_valid))
+
+
+class _NumberCheck:
+    """
+    Class used for validating entry widgets, self.vcmd is provided as the validatecommand
+    """
+
+    def __init__(self, parent, min_, max_, current):
+        self.parent = parent
+        self.low = min_
+        self.high = max_
+        self.vcmd = parent.register(self.in_integer_range), '%d', '%P'
+
+        if _NumberCheck.in_range(0, min_, max_):
+            self.last_valid = 0
+        else:
+            self.last_valid = min_
+        if current is not None:
+            self.last_valid = current
+
+    def in_integer_range(self, type_, after_text):
+        """
+        Validates an entry to make sure the correct text is being inputted
+        :param type_:        0 for deletion, 1 for insertion, -1 for focus in
+        :param after_text:   The text that the entry will display if validated
+        :return:
+        """
+
+        if type_ == '-1':
+            if _is_int(after_text):
+                self.last_valid = int(after_text)
+
+        # Delete Action, always okay, if valid number save it
+        elif type_ == '0':
+            try:
+                num = int(after_text)
+                self.last_valid = num
+            except ValueError:
+                pass
+            return True
+
+        # Insert Action, okay based on ranges, if valid save num
+        elif type_ == '1':
+            try:
+                num = int(after_text)
+            except ValueError:
+                if self.can_be_negative() and after_text == '-':
+                    return True
+                return False
+            if self.is_valid_range(num):
+                self.last_valid = num
+                return True
+            return False
+        return False
+
+    def can_be_negative(self):
+        """
+        Tests whether this given entry widget can have a negative number
+
+        :return: Whether or not the entry can have a negative number
+        """
+        return (self.low is None) or (self.low < 0)
+
+    def is_valid_range(self, num):
+        """
+        Tests whether the given number is valid for this entry widgets range
+
+        :param num: The number to range test
+
+        :return: Whether or not the number is in range
+        """
+        return _NumberCheck.in_range(num, self.low, self.high)
+
+    @staticmethod
+    def in_range(num, low, high):
+        """
+        Tests whether or not a number is within a specified range inclusive
+
+        :param num: The number to test if its in the range
+        :param low: The minimum of the range
+        :param high: The maximum of the range
+
+        :return: Whether or not the number is in the range
+        """
+        if (low is not None) and (num < low):
+            return False
+        if (high is not None) and (num > high):
+            return False
+        return True
+
+if __name__ == '__main__':
+    import tkinter as tk
+    from tkinter import ttk
+
+    root = tk.Tk()
+    var = tk.DoubleVar()
+    widget = ttk.Spinbox(root, textvariable=var, justify=tk.CENTER, from_=0, to_=10)
+    widget.pack(padx=10, pady=10)
+    int_validate(widget, limits=(0, 10))
+    root.mainloop()
--- src/viewers/tkviewer2d.py
+++ src/viewers/tkviewer2d.py
@@ -3,32 +3,38 @@ import tkinter as tk
 import numpy as np
 
 from src.simulation.blockarray import BlockArray
+from viewers.integercheck import int_validate
 
 
-class TkViewer2d(ttk.Frame):
+class TkViewer2dGui(ttk.Frame):
     LABEL_TEXT = 'Time: {:.1f}'
-    def __init__(self, parent, rows, cols, solution, time_interval=.1, spring_length=1, velocity=.1, scale=30, block_size=10, **kwargs):
+    def __init__(self, parent, rows, cols, solution, time_interval=.1, spring_length=1,  **kwargs):
         ttk.Frame.__init__(self, parent, **kwargs)
+        self.time = 0
         self.rows = rows
         self.cols = cols
         self.solution = solution
-        self.time = 0
         self.spring_length = spring_length
-        self.velocity = velocity
+        self.time_interval = time_interval
+
+        self.sidebar = Sidebar(self)
+        self.separator = ttk.Separator(self, orient=tk.VERTICAL)
         self.time_label = ttk.Label(self, text=self.LABEL_TEXT.format(self.time))
         self.canvas = tk.Canvas(self)
-        self.scale = scale
-        self.block_size = block_size
-        self.time_interval = time_interval
+
+        self.sidebar.pack(fill=tk.Y, side=tk.RIGHT)
+        self.separator.pack(fill=tk.Y, padx=5, pady=10, side=tk.RIGHT)
         self.time_label.pack()
         self.canvas.pack(expand=tk.YES, fill=tk.BOTH)
 
+        self.update_values()
+
     def draw_blocks(self, positions):
-        width = self.winfo_width()
-        height = self.winfo_height()
+        width = self.canvas.winfo_width()
+        height = self.canvas.winfo_height()
         distance = (self.spring_length * self.scale)
-        start_x = (width - (self.rows-1)*distance - self.block_size) / 2
-        start_y = (height - (self.cols-1)*distance - self.block_size) / 2
+        start_x = (width - (self.cols-1)*distance - self.block_size) / 2
+        start_y = (height - (self.rows-1)*distance - self.block_size) / 2
         block_array = BlockArray(positions, self.cols)
         for i in range(self.rows):
             for j in range(self.cols):
@@ -37,22 +43,80 @@ class TkViewer2d(ttk.Frame):
                 self.canvas.create_rectangle(x, y, x + self.block_size, y + self.block_size)
 
     def draw_recursive(self, i):
-        self.time += self.time_interval
+        self.update_values()
         self.time_label.config(text=self.LABEL_TEXT.format(self.time))
         self.canvas.delete(tk.ALL)
         self.draw_blocks(np.array(self.solution[i]))
         if i < len(self.solution) - 1:
-            self.after(int(self.time_interval * 200), lambda: self.draw_recursive(i + 1))
+            self.after(int(self.time_interval * self.wait_time), lambda: self.draw_recursive(i + 1))
+        else:
+            self.sidebar.button_start.state(['!disabled'])
+        self.time += self.time_interval
+
+    def update_values(self):
+        try:
+            self.wait_time = 1000 // self.sidebar.speed_var.get()
+        except tk.TclError:
+            pass
+        try:
+            self.scale = self.sidebar.scale_var.get()
+        except tk.TclError:
+            pass
+        try:
+            self.block_size = self.sidebar.block_size_var.get()
+        except tk.TclError:
+            pass
 
     def start(self):
-        self.time = 0 - self.time_interval
+        self.time = 0
         self.draw_recursive(0)
 
 
-def view_2d(rows, cols, solution, velocity):
+class Sidebar(ttk.Frame):
+    def __init__(self, *args, **kwargs):
+        ttk.Frame.__init__(self, *args, **kwargs)
+        self.block_size_var = tk.IntVar()
+        block_size_label = ttk.Label(self, text='Block Size')
+        block_size_spinbox = ttk.Spinbox(self, textvariable=self.block_size_var, justify=tk.CENTER, from_=2, to_=30)
+        int_validate(block_size_spinbox, (2, 30))
+        self.block_size_var.set(10)
+
+        self.scale_var = tk.IntVar()
+        scale_label = ttk.Label(self, text='Scale')
+        scale_spinbox = ttk.Spinbox(self, textvariable=self.scale_var, justify=tk.CENTER, from_=5, to_=100)
+        int_validate(scale_spinbox, (5, 50))
+        self.scale_var.set(30)
+
+        self.speed_var = tk.IntVar()
+        speed_label = ttk.Label(self, text='Speed')
+        speed_spinbox = ttk.Spinbox(self, textvariable=self.speed_var, justify=tk.CENTER, from_=1, to_=30)
+        int_validate(speed_spinbox, (5, 50))
+        self.speed_var.set(1)
+
+        self.button_start = ttk.Button(self, text='Start')
+
+        block_size_label.pack()
+        block_size_spinbox.pack(pady=(0, 10))
+        scale_label.pack()
+        scale_spinbox.pack(pady=(0, 10))
+        speed_label.pack()
+        speed_spinbox.pack(pady=(0, 10))
+        self.button_start.pack()
+
+
+class TkViewer:
+    def __init__(self, *args, **kwargs):
+        self.gui = TkViewer2dGui(*args, **kwargs)
+        self.gui.sidebar.button_start.config(command=self.on_start)
+
+    def on_start(self):
+        self.gui.sidebar.button_start.state(['disabled'])
+        self.gui.start()
+
+
+def view_2d(rows, cols, solution):
     root = tk.Tk()
-    viewer = TkViewer2d(root, rows, cols, solution, velocity=velocity)
-    button = ttk.Button(root, text='Start', command=lambda: viewer.start())
-    viewer.pack(expand=tk.YES, fill=tk.BOTH)
-    button.pack()
+    root.wm_title('Block Viewer')
+    viewer = TkViewer(root, rows, cols, solution)
+    viewer.gui.pack(expand=tk.YES, fill=tk.BOTH)
     root.mainloop()
