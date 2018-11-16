e9e89626a0faccb5118cb7fe26816bff3b2dd7e9
Vincent <vince.shores@outlook.com>
2018-11-15 22:21:39 -0500

    Put common functionality for main files in util file

---
+++ src/files/util.py
@@ -0,0 +1,40 @@
+import glob
+import os
+import sys
+
+from files.saveables import Data
+
+def read_data(file_name):
+    with open(file_name, 'rb') as file:
+        data, index =  Data.from_byte_array(bytearray(file.read()))
+        return data
+
+def write_data(file_name, data):
+    with open(file_name, 'wb') as file:
+        file.write(data.to_byte_array())
+
+def get_file_name():
+    if len(sys.argv) == 1:
+        files = glob.iglob('data/*')
+        return max(files, key=os.path.getctime)
+    elif len(sys.argv) == 2:
+        return os.path.join('data', sys.argv[1])
+    else:
+        raise TypeError('Usage: [filename]')
+
+def data_desc(data):
+        return ('rows: {}\n' +
+               'cols: {}\n' +
+               'm:    {}\n' +
+               'v:    {}\n' +
+               'l:    {}\n' +
+               'k_b:  {}\n' +
+               'k_p:  {}\n' +
+               'u_s:  {}\n' +
+               'u_k:  {}\n' +
+               'dt:   {}').format(data.rows.get(), data.cols.get(), data.mass.get(),
+                               data.plate_velocity.get(),
+                               data.spring_length.get(),
+                               data.spring_constant.get(), data.plate_spring_constant.get(),
+                               data.static_friction.get(), data.kinetic_friction.get(),
+                               data.time_interval.get())
--- src/mains/main_block_viewer.py
+++ src/mains/main_block_viewer.py
@@ -1,34 +1,7 @@
-import glob
-import os
-import sys
-import numpy as np
-
-from files.readwrite import read_data
+from files import util
 from viewers.tkviewer import view_2d
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        files = glob.iglob('data/*')
-        file = max(files, key=os.path.getctime)
-    elif len(sys.argv) == 2:
-        file = os.path.join('data', sys.argv[1])
-    else:
-        raise TypeError('Usage: [filename]')
-
-    data = read_data(file)
-    desc = ('rows: {}\n' +
-            'cols: {}\n' +
-            'm:    {}\n' +
-            'v:    {}\n' +
-            'l:    {}\n' +
-            'k_b:  {}\n' +
-            'k_p:  {}\n' +
-            'u_s:  {}\n' +
-            'u_k:  {}\n' +
-            'dt:   {}').format(data.rows.get(), data.cols.get(), data.mass.get(),
-                               data.plate_velocity.get(),
-                               data.spring_length.get(),
-                               data.spring_constant.get(), data.plate_spring_constant.get(),
-                               data.static_friction.get(), data.kinetic_friction.get(),
-                               data.time_interval.get())
-    view_2d(data, desc)
+    file = util.get_file_name()
+    data = util.read_data(file)
+    view_2d(data, util.data_desc(data))
