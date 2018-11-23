00e0affe215fdfc304d304dc2ed84a93e9682664
Vincent <vince.shores@outlook.com>
2018-11-23 17:12:59 -0500

    Allow initial steps without data collection to equilibrate

--- src/simulation/simulation.py
+++ src/simulation/simulation.py
@@ -5,7 +5,7 @@ from scipy.integrate import ode
 from simulation.blockarray import BlockArray
 
 
-def solve(data, differential, steps):
+def solve(data, differential, initial_steps, steps):
     print('Creating initial data')
     blocks = BlockArray(data.rows, data.cols)
     for i in range(data.rows):
@@ -15,15 +15,19 @@ def solve(data, differential, steps):
     r = ode(differential)
     r.set_integrator('dopri5', nsteps=10000)
     r.set_initial_value(blocks.array)
-    progress_at = 0
     start = datetime.now().timestamp()
-    while r.successful() and r.t < steps * data.time_interval:
+    progress_at = start + 3
+    for step in range(initial_steps + steps):
         values = r.integrate(r.t+data.time_interval)
-        data.add_slice(r.t, values)
-        if r.t > progress_at:
-            current = datetime.now().timestamp() - start
-            print('Time-step: {}, Real-time: {:.2f}s'.format(progress_at, current))
-            progress_at += 1
+        if step > initial_steps:
+            data.add_slice(r.t, values)
+        current = datetime.now().timestamp()
+        if current > progress_at:
+            print('Initial steps: {}, Step: {}, Time: {}, Real-time: {:.2f}s'.format(initial_steps,
+                                                                                     step, r.t, current - start))
+            progress_at = current + 3
+        if not r.successful():
+            break
     elapsed = datetime.now().timestamp() - start
     data.total_time = elapsed
     print('Finished at: {:.2f}s'.format(elapsed))
