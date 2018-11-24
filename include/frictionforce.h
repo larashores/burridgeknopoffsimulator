b1c90e01dc9202e63e104dd1af4bf5cd6cb8b478
Vincent <vince.shores@outlook.com>
2018-11-24 00:29:41 -0500

    Added scaled friction force

--- include/frictionforce.h
+++ include/frictionforce.h
@@ -1,3 +1,5 @@
 #pragma once
 
 double friction_force(double velocity, double static_coefficient, double kinetic_coefficient, double mass);
+
+double scaled_friction_force(double velocity, double alpha);
--- src/frictionforce.cpp
+++ src/frictionforce.cpp
@@ -1,8 +1,18 @@
 #include "frictionforce.h"
 
+#include <cmath>
 
-constexpr double epsilon {1e-2};
+
+constexpr double epsilon {1e-3};
 constexpr double g {9.81};
+
+namespace {
+    template <typename T>
+    constexpr int sign(T val)
+    {
+        return (T{0} < val) - (val < T{0});
+    }
+}
 
 double friction_force(double velocity, double static_coefficient, double kinetic_coefficient, double mass)
 {
@@ -18,5 +28,16 @@ double friction_force(double velocity, double static_coefficient, double kinetic
         double y1 {1 / (kinetic_coefficient * epsilon + (1 / cutoff))};
         double y2 {- 1 / (kinetic_coefficient * epsilon + (1 / cutoff))};
         return (y2-y1)*(velocity+epsilon)/(2*epsilon) + y1;
+    }
+}
+
+double scaled_friction_force(double velocity, double alpha)
+{
+    if (velocity < -epsilon or velocity > epsilon)
+    {
+        return - sign(velocity) / (2*alpha*std::abs(velocity) + 1);
+    } else
+    {
+        return - (velocity / (2*alpha*epsilon + 1)) / epsilon;
     }
 }
