985cb2a25d9b62d695b14535ff60223804a1a1ff
Vincent <vince.shores@outlook.com>
2018-11-24 02:22:40 -0500

    Python bindings created

--- src/python.cpp
+++ src/python.cpp
@@ -1,6 +1,9 @@
 #include <boost/python.hpp>
 #include <boost/python/numpy.hpp>
 
+#include "differentials/positionupdater.h"
+#include "differentials/scaledfrictionalforce.h"
+#include "differentials/scaledplateforce.h"
 #include "differentials/scaledspringforce.h"
 #include "differentials/springforce.h"
 #include "frictionforce.h"
@@ -16,6 +19,12 @@ BOOST_PYTHON_MODULE(burridgeknopoff)
             .def("__call__", &SpringForce::differentiate);
     PY::class_<ScaledSpringForce>("ScaledSpringForce", PY::init<int, int, double, double>())
             .def("__call__", &ScaledSpringForce::differentiate);
+    PY::class_<ScaledFrictionalForce>("ScaledFrictionalForce", PY::init<int, int, double, double>())
+            .def("__call__", &ScaledFrictionalForce::differentiate);
+    PY::class_<ScaledPlateForce>("ScaledPlateForce", PY::init<int, int, double>())
+            .def("__call__", &ScaledPlateForce::differentiate);
+    PY::class_<PositionUpdater>("PositionUpdater", PY::init<int, int>())
+            .def("__call__", &PositionUpdater::differentiate);
 
     PY::def("friction_force", friction_force);
     PY::def("scaled_friction_force", scaled_friction_force);
