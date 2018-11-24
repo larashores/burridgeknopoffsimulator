440cc357c6b0af136c2f90baffaee082c986761c
Vincent <vince.shores@outlook.com>
2018-11-24 02:43:59 -0500

    Use BurridgeKnopoffLib for all of scaleddifferential

--- src/simulation/scaleddifferential.py
+++ src/simulation/scaleddifferential.py
@@ -1,15 +1,15 @@
-from simulation.scaledplateforce import ScaledPlateForce
-from simulation.scaledfrictionforce import ScaledFrictionalForce
-from simulation.positionupdater import PositionUpdater
-from simulation.scaledspringforce import ScaledSpringForce
+import numpy as np
+import burridgeknopoff as bk
 
 
 class ScaledDifferential:
     def __init__(self, num_rows, num_cols, *, scaled_spring_length, scaled_plate_velocity, alpha, l):
-        self._velocity = PositionUpdater(num_rows, num_cols)
-        self._spring_force = ScaledSpringForce(num_rows, num_cols, scaled_spring_length, l)
-        self._driving_plate_force = ScaledPlateForce(num_rows, num_cols, scaled_spring_length)
-        self._frictional_force = ScaledFrictionalForce(num_rows, num_cols, scaled_plate_velocity, alpha)
+        self._velocity = bk.PositionUpdater(num_rows, num_cols)
+        self._spring_force = bk.ScaledSpringForce(num_rows, num_cols, scaled_spring_length, l)
+        self._driving_plate_force = bk.ScaledPlateForce(num_rows, num_cols, scaled_spring_length)
+        self._frictional_force = bk.ScaledFrictionalForce(num_rows, num_cols, scaled_plate_velocity, alpha)
+        self._rows = num_rows
+        self._cols = num_cols
 
     def __call__(self, t, values):
         """
@@ -18,9 +18,9 @@ class ScaledDifferential:
 
         Function should return a list of values [
         """
-        spring_force = self._spring_force.diff(values)
-        friction_force = self._frictional_force(values)
-        plate_force = self._driving_plate_force(values)
-        new_positions = self._velocity(values)
-        net = spring_force + new_positions + friction_force + plate_force
+        net = np.zeros(self._rows * self._cols * 2)
+        self._spring_force(values, net)
+        self._frictional_force(values, net)
+        self._driving_plate_force(values, net)
+        self._velocity(values, net)
         return net
