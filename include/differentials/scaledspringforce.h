7b4f3a373b9995ffcc7a248b02c5728abc061bb5
Vincent <vince.shores@outlook.com>
2018-11-24 02:09:03 -0500

    Spring force now works for any dimension

--- include/differentials/scaledspringforce.h
+++ include/differentials/scaledspringforce.h
@@ -11,8 +11,12 @@ public:
     boost::python::numpy::ndarray differentiate(boost::python::numpy::ndarray& current_array) const;
 
 private:
+    boost::python::numpy::ndarray diff_full(boost::python::numpy::ndarray& current_array) const;
+    boost::python::numpy::ndarray diff_one_dim(boost::python::numpy::ndarray& current_array) const;
+
     const int m_rows;
     const int m_cols;
     const double m_spring_length;
     const double m_l_squared;
+    std::function<boost::python::numpy::ndarray(boost::python::numpy::ndarray&)> m_diff_func;
 };
--- src/differentials/scaledspringforce.cpp
+++ src/differentials/scaledspringforce.cpp
@@ -12,18 +12,59 @@ ScaledSpringForce::ScaledSpringForce(int rows, int cols, double spring_length, d
         m_spring_length{spring_length},
         m_l_squared{l*l}
 {
+    if (rows == 1 and cols >= 2)
+    {
+        m_diff_func = [this](auto current){return diff_one_dim(current);};
+    } else
+    {
+        m_diff_func = [this](auto current){return diff_full(current);};
+    }
 }
 
 boost::python::numpy::ndarray ScaledSpringForce::differentiate(NP::ndarray& current_ndarray) const
+{
+    return m_diff_func(current_ndarray);
+}
+
+boost::python::numpy::ndarray ScaledSpringForce::diff_full(NP::ndarray& current_ndarray) const
+{
+    auto results_ndarray {NP::zeros(boost::python::make_tuple(m_rows*m_cols*2), NP::dtype::get_builtin<double>())};
+    auto results {reinterpret_cast<double*>(results_ndarray.get_data())};
+    auto current {reinterpret_cast<double*>(current_ndarray.get_data())};
+
+    for(int i=0; i < m_rows; i++)
+    {
+        for(int j=0; j < m_cols; j++)
+        {
+            double cur {get_position(current, m_cols, i, j)};
+
+            double force {0};
+            if(i >= 1) {
+                force += get_position(current, m_cols, i - 1, j) - cur;
+            } if(i < m_rows - 1) {
+                force += get_position(current, m_cols, i + 1, j) - cur;
+            } if(j >= 1) {
+                force += get_position(current, m_cols, i, j - 1) - cur + m_spring_length;
+            } if(j < m_cols - 1) {
+                force += get_position(current, m_cols, i, j + 1) - cur - m_spring_length;
+            }
+            set_velocity(results, m_cols, i, j, force * m_l_squared);
+        }
+    }
+
+    return results_ndarray;
+}
+
+boost::python::numpy::ndarray ScaledSpringForce::diff_one_dim(NP::ndarray& current_ndarray) const
 {
     auto results_ndarray {NP::zeros(boost::python::make_tuple(m_rows*m_cols*2), NP::dtype::get_builtin<double>())};
     auto results {reinterpret_cast<double*>(results_ndarray.get_data())};
     auto current {reinterpret_cast<double*>(current_ndarray.get_data())};
 
     set_velocity(results, 0,
-                    m_l_squared * (get_position(current, 1) - get_position(current, 0) - m_spring_length));
+                 m_l_squared * (get_position(current, 1) - get_position(current, 0) - m_spring_length));
     set_velocity(results, m_cols - 1, m_l_squared *
-                    (get_position(current, m_cols-2) - get_position(current, m_cols-1) + m_spring_length));
+                                      (get_position(current, m_cols-2) - get_position(current, m_cols-1) + m_spring_length));
     for(int j=1; j < m_cols - 1; j++)
     {
         double force{m_l_squared * (get_position(current, j - 1)
