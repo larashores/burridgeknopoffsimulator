be2b2b3508718de2c10da075b495b38925d4882f
Vincent <vince.shores@outlook.com>
2018-12-23 16:31:03 -0500

    Add C++ differential

---
+++ include/differentials/c/cdifferential.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "differentials/c/cfrictionalforce.h"
+#include "differentials/c/cplateforce.h"
+#include "differentials/c/cpositionupdater.h"
+#include "differentials/c/cspringforce.h"
+
+#include <valarray>
+
+class CDifferential
+{
+public:
+    CDifferential(int num_rows, int num_cols,
+                  double scaled_spring_length,
+                  double scaled_plate_velocity,
+                  double alpha, double l);
+
+    std::valarray<double> differentiate(double time, const std::valarray<double>& values) const;
+private:
+    CPositionUpdater m_position_updater;
+    CSpringForce m_spring_force;
+    CPlateForce m_plate_force;
+    CFrictionalForce m_frictional_force;
+    const int m_rows;
+    const int m_cols;
+};
