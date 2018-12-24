737b85d77107f6ef704d0e760aba27e0f68dadf8
Vincent <vince.shores@outlook.com>
2018-12-24 03:37:03 -0500

    Delete outdated numpy files

--- src/odesolver/rungekutta4.cpp
+++
@@ -1,9 +0,0 @@
-#include "odesolver/rungekutta4.h"
-
-RungeKutta4::RungeKutta4(OdeSolver::FuncType difeqs) :
-        RungeKutta{{0, .5, .5, 1},
-                   {1, 2, 2, 1},
-                   difeqs}
-{
-
-}
