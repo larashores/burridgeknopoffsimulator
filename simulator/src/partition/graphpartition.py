3797a7ff155df5108782cd85ef881545862efe3d
Vincent <vince.shores@outlook.com>
2019-02-06 12:47:48 -0500

    Calculate distance for slip events

--- simulator/src/partition/graphpartition.py
+++ simulator/src/partition/graphpartition.py
@@ -5,17 +5,30 @@ import math
 
 
 class SlipEvent:
-    def __init__(self, coord):
-        self._start_time, row, col = coord
+    def __init__(self, data, coord):
+        self._data = data
+        self._blocks = set()
+        self._start_time = None
         self._end_time = None
-        self._blocks = [coord]
+        self.add_block(*coord)
 
     def add_block(self, time, row, col):
-        self._blocks.append((time, row, col))
-        self._end_time = time
+        self._blocks.add((row, col))
+        self._start_time = min(self._start_time, time) if self._start_time is not None else time
+        self._end_time = max(self._end_time, time) if self._end_time is not None else time
+
+    @property
+    def distance(self):
+        dist = 0
+        for row, col in self._blocks:
+            start_block_array = BlockArray(self._data.values_list[self._start_time].get(), self._data.run_info.cols)
+            end_block_array = BlockArray(self._data.values_list[self._end_time].get(), self._data.run_info.cols)
+            dist += end_block_array.positions[row, col] - start_block_array.positions[row, col]
+        return dist
 
     def __str__(self):
-        return f'Start:{self._start_time}, End: {self._end_time}, {sorted(self._blocks)}'
+        return f'Start:{self._start_time}, End: {self._end_time}, distance: {self.distance} '  \
+               f'{sorted(list(self._blocks))}'
 
 
 class GraphParitioner:
@@ -33,7 +46,7 @@ class GraphParitioner:
         slipped_blocks = self._slipped_blocks()
         while len(slipped_blocks) > 0:
             coord = slipped_blocks.pop()
-            slip_event = SlipEvent(coord)
+            slip_event = SlipEvent(self._data, coord)
             self._depth_first_search(slip_event, slipped_blocks, coord)
             components.append(slip_event)
         components.sort(key=lambda slip: slip._start_time)
